dbc91272940a614053e4e997f59649ac90a0768f
Julio Cesar <cesarjulio1475@gmail.com>
2024-05-03 12:14:14 -0300

    Estudando exemplo de entrada de dados via "import" scanner pelo terminal

--- dio-estudo-exemplo-args-java/src/Args.java
+++ dio-estudo-exemplo-args-java/src/Args.java
@@ -1,10 +1,25 @@
+    import java.util.Scanner;
+    import java.util.Locale;
+    
     public class Args {
         public static void main(String[] args) {
-            //os argumentos começam com indice 0
-            String nome = args [0];
-            String sobrenome = args [1];
-            int idade = Integer.valueOf(args[2]);
-            double altura = Double.valueOf(args[3]);
+            //criando o objeto scanner
+            Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+            System.out.println("Digite seu nome");
+            String nome = scanner.next();
+
+            System.out.println("Digite seu sobrenome");
+            String sobrenome = scanner.next();
+
+            System.out.println("Digite sua idade");
+            int idade = scanner.nextInt();
+
+            System.out.println("Digite sua altura");
+            double altura = scanner.nextDouble();
+
+
+           
     
             System.out.println("Ola, me chamo " + nome + " "+ sobrenome);
             System.out.println("Tenho " + idade + "anos");
